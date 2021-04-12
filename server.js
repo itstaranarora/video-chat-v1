562f511d2f8cfde3ca7fad410ba08c11a92fe55a
Igor Agapov <59837819+harryheman@users.noreply.github.com>
2021-04-13 03:49:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.param.room });
+  res.render("room", { roomId: req.params.room });
 });
 
 io.on("connection", (socket) => {
