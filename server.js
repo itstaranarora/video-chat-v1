9bd1a055d77b7a2edb6879325eb0424c9a4c7184
Igor Agapov <59837819+harryheman@users.noreply.github.com>
2021-04-13 04:10:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
